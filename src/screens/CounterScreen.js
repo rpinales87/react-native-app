2667ad196e3189b474a3b90c28ae8089feaca6d2
rpinales87 <pinales.roberto@gmail.com>
2019-09-02 11:38:47 -0400

    Created:
    - TextScreen: Added link to HomeScreen and updated App.js to render
    
    Updated:
    - CounterScreen to use reducer instead of useState.
    - SquareScreen to meet community conventions.(Also created a copy
      with previous version for future reference.)

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+const reducer = (state, action) => {
+  // state === { count: number }
+ // action === { type: 'increment' || 'decrement', payload: 1 }
+
+ switch (action.type) {
+   case 'increment':
+    return { ...state, count: state.count + action.payload }
+   case 'decrement':
+    return { ...state, count: state.count - action.payload }
+   default:
+    return state
+ }
+}
+
 const CounterScreen = () => {
-  // let counter = 0
-  const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
 
   return (
     <View>
       <Button title="Increase" onPress={() => {
-        // counter++
-        // console.log('Increase counter is:', counter)
-        setCounter(counter + 1)
-      }} />
+          dispatch({ type: 'increment', payload: 1 })
+        }} />
       <Button title="Decrease" onPress={() => {
-        // counter--
-        // console.log('Decrease counter is:', counter)
-        setCounter(counter - 1)
-      }} />
-      <Text>Current Count: {counter}</Text>
+          dispatch({ type: 'decrement', payload: 1 })
+        }} />
+      <Text>Current Count: {state.count}</Text>
     </View>
   )
 }
